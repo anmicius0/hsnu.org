47ec1dfb940ea67b98485926bd240f0df6832c3e
Anmicius <anmicius@localhost.localdomain>
2020-02-14 15:46:26 +0800

    save before connect to wordpress

--- src/components/section/Article/Article.js
+++ src/components/section/Article/Article.js
@@ -10,9 +10,14 @@ import RecommandVideo from "../../tools/RecommandVideo/RecommandVideo"
 
 const Article = () => {
   const [news, setNews] = useState([1, 2, 3, 4, 5])
+
+  // when scroll near the bottom, add news
   useEffect(() => {
     window.onscroll = function(ev) {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight / 7
+      ) {
         // you're at the bottom of the page
         setTimeout(() => {
           setNews(news.concat([1, 2, 3, 4, 5]))

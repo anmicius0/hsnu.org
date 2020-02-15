891f42c571fdd8444fc168eae7b13b44fb74f9cd
Anmicius <anmicius@localhost.localdomain>
2020-02-15 10:28:58 +0800

    connected to wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,17 @@ module.exports = {
         path: `./src/data/`,
       },
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "wordpress",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "wordpress",
+        // Url to query from
+        url: "http://hsnu.local/graphql",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

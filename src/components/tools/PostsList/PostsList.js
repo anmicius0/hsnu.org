c163a8110becdab937a99117c0362592c38601bf
Anmicius <anmicius@localhost.localdomain>
2020-02-15 18:22:07 +0800

    togglegenres

--- src/components/tools/PostsList/PostsList.js
+++ src/components/tools/PostsList/PostsList.js
@@ -1,39 +1,70 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { Container, Row, Col, Nav } from "react-bootstrap"
 
 import "./Postslist.scss"
 const PostsList = () => {
+  // query
+  const result = useStaticQuery(graphql`
+    query {
+      wordpress {
+        sposts {
+          edges {
+            node {
+              title(format: RENDERED)
+            }
+          }
+        }
+      }
+    }
+  `).wordpress.sposts.edges
+
+  // toggle tabs
+  useEffect(() => {
+    const genres = document.querySelectorAll(".genre:not(.more)")
+    for (var i = 0; i < genres.length; i++) {
+      genres[i].addEventListener("click", e => {
+        // clear all .active
+        for (var i = 0; i < genres.length; i++) {
+          if (genres[i].classList.contains("active")) {
+            genres[i].classList.remove("active")
+          }
+        }
+
+        e.target.parentElement.classList.add("active")
+      })
+    }
+  }, [])
+
   return (
     <div className={"posts"}>
       {/* genres */}
       <Nav variant="tabs" className={"genres"}>
-        <Nav.Item className={"genre active"}>
-          <h3 className={"is-3"}>學生事務</h3>
-        </Nav.Item>
-        {[1, 2, 3].map(utem => (
-          <Nav.Item className={"genre"}>
-            <h3 className={"is-3"}>學生事務</h3>
-          </Nav.Item>
-        ))}
+        {["學生事務", "研習", "競賽", "教師"].map((item, index) =>
+          index === 0 ? (
+            <Nav.Item className={"genre active"}>
+              <h3 className={"is-3"}>{item}</h3>
+            </Nav.Item>
+          ) : (
+            <Nav.Item className={"genre"}>
+              <h3 className={"is-3"}>{item}</h3>
+            </Nav.Item>
+          )
+        )}
         <Nav.Item className={"genre more"}>
           <h3 className={"is-3"}>更多...</h3>
         </Nav.Item>
       </Nav>
 
-      {/*  */}
       <Container>
         <Row className={"flex-column"}>
-          {[1, 2, 3, 4, 5, 6, 7, 8].map(item => (
+          {result.map(item => (
             <Col className={"post"}>
               <a href="#">
-                <h4 className={"is-4"}>
-                  108學年度師大附中2019/20年「香港大學校長推薦計劃」推薦辦法
-                  108學年度師大附中2019/20年「香港大學校長推薦計劃」推薦辦法
-                  108學年度師大附中2019/20年「香港大學校長推薦計劃」推薦辦法
-                  108學年度師大附中2019/20年「香港大學校長推薦計劃」推薦辦法
-                  108學年度師大附中2019/20年「香港大學校長推薦計劃」推薦辦法
-                  108學年度師大附中2019/20年「香港大學校長推薦計劃」推薦辦法
-                </h4>
+                <h4
+                  className={"is-4"}
+                  dangerouslySetInnerHTML={{ __html: item.node.title }}
+                />
               </a>
             </Col>
           ))}

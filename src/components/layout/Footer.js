74bdacf9954f71c56f5e2dac39738c7168f1eeb1
Anmicius <anmicius@localhost.localdomain>
2020-02-26 08:52:08 +0800

    fix footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,20 +4,29 @@ import { Navbar, Nav, Container, Row, Col } from "react-bootstrap"
 // icons
 import medium from "../../images/icons/socialMedia/medium.svg"
 import twitter from "../../images/icons/socialMedia/twitter.svg"
+import github from "../../images/icons/socialMedia/github.svg"
+import patreon from "../../images/icons/socialMedia/patreon.svg"
 
 const Footer = () => {
+  const footers = [
+    { name: "twitter", link: "https://twitter.com", icon: twitter },
+    { name: "medium", link: "https://medium.com", icon: medium },
+    { name: "github", link: "https://github.com", icon: github },
+    { name: "patreon", link: "https://patreon.com", icon: patreon },
+  ]
+
   return (
     <>
       <footer>
         <Container fluid>
           <Row>
-            <Col lg="8" className={"logo"}>
+            <Col lg="7" className={"logo"}>
               <h3 className={"is-3 bold"}>We Are HSNUer</h3>
             </Col>
-            <Col lg="4" className={"icons"}>
-              {[medium, twitter, medium, twitter, medium, twitter].map(icon => (
-                <a href="#">
-                  <img src={icon}></img>
+            <Col lg="5" className={"icons"}>
+              {footers.map(icon => (
+                <a href={icon.link}>
+                  <img src={icon.icon} alt={icon.name}></img>
                 </a>
               ))}
             </Col>

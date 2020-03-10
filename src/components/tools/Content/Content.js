5747452166735f022c6df08f6184abcfd9f1113d
Anmicius <anmicius@localhost.localdomain>
2020-03-10 14:47:54 +0800

    identify file name

--- src/components/tools/Content/Content.js
+++ src/components/tools/Content/Content.js
@@ -1,60 +1,31 @@
 import React from "react"
 
 import "./Content.scss"
-import pdf from "../../../images/icons/files/pdf.svg"
+import Files from "../Files/Files"
 
-const Content = props => {
+const Content = ({ title, genre, date, content, files, urls }) => {
   return (
     <article className={"content"}>
       {/* title */}
       <h2
         className={"is-2 bold serif"}
-        dangerouslySetInnerHTML={{ __html: props.title }}
+        dangerouslySetInnerHTML={{ __html: title }}
       />
 
       {/* meta */}
       <div className={"meta"}>
-        <p className={"is-5 light"}>{props.genre}</p>
-        <p className={"is-5 light"}>{props.date}</p>
+        <p className={"is-5 light"}>{genre}</p>
+        <p className={"is-5 light"}>{date}</p>
         <p className={"is-5 tag"}>熱門</p>
       </div>
 
       {/* files */}
-      <div className={"files"}>
-        {props.files
-          ? props.files.map(file => (
-              <>
-                <a href={file.file.url.source_url}>
-                  <div className={"file pdf"}>
-                    {/* icon */}
-                    <img src={pdf}></img>
-                    {/* name */}
-                    <p className={"is-5"}>{file.file.filename}</p>
-                  </div>
-                </a>
-              </>
-            ))
-          : null}
-        {props.urls
-          ? props.urls.map(url => (
-              <>
-                <a href={url.url}>
-                  <div className={"file pdf"}>
-                    {/* icon */}
-                    <img src={pdf}></img>
-                    {/* name */}
-                    <p className={"is-5"}>{url.description}</p>
-                  </div>
-                </a>
-              </>
-            ))
-          : null}
-      </div>
+      <Files files={files} urls={urls}></Files>
 
       {/* context */}
       <p
         className={"is-4 context"}
-        dangerouslySetInnerHTML={{ __html: props.content }}
+        dangerouslySetInnerHTML={{ __html: content }}
       />
     </article>
   )
---
+++ src/components/tools/Files/Files.js
@@ -0,0 +1,92 @@
+import React from "react"
+
+import "./File.scss"
+
+import pdf from "../../../images/icons/files/pdf.svg"
+import word from "../../../images/icons/files/word.svg"
+import excel from "../../../images/icons/files/excel.svg"
+import ppt from "../../../images/icons/files/ppt.svg"
+import link from "../../../images/icons/files/link.svg"
+
+const Content = ({ files, urls }) => {
+  const pdf_extension = ["pdf"]
+  const word_extension = ["doc", "docx", "odt", "txt", "fodt"]
+  const ppt_extension = ["ppt", "pptx", "odp", "fodp"]
+  const excel_extension = ["xlsx", "xls", "ods", "fods"]
+
+  //   identify file type
+  function identity_file(file_name) {
+    const filename_array = file_name.split(".")
+    const extension = filename_array[filename_array.length - 1]
+
+    if (pdf_extension.indexOf(extension) !== -1) {
+      return "pdf"
+    } else if (word_extension.indexOf(extension) !== -1) {
+      return "word"
+    } else if (ppt_extension.indexOf(extension) !== -1) {
+      return "ppt"
+    } else if (excel_extension.indexOf(extension) !== -1) {
+      return "excel"
+    } else {
+      return "link"
+    }
+  }
+
+  //    get file icon
+  function get_icon(file_name) {
+    const filename_array = file_name.split(".")
+    const extension = filename_array[filename_array.length - 1]
+
+    if (pdf_extension.indexOf(extension) !== -1) {
+      return pdf
+    } else if (word_extension.indexOf(extension) !== -1) {
+      return word
+    } else if (ppt_extension.indexOf(extension) !== -1) {
+      return ppt
+    } else if (excel_extension.indexOf(extension) !== -1) {
+      return excel
+    } else {
+      return link
+    }
+  }
+
+  return (
+    <>
+      {/* files */}
+      <div className={"files"}>
+        {files
+          ? files.map(file => (
+              <>
+                <a href={file.file.url.source_url}>
+                  <div className={`file ${identity_file(file.file.filename)}`}>
+                    {/* icon */}
+                    <img src={get_icon(file.file.filename)}></img>
+                    {/* name */}
+                    <p className={"is-5"}>{file.file.filename}</p>
+                  </div>
+                </a>
+              </>
+            ))
+          : null}
+
+        {/* render urls */}
+        {urls
+          ? urls.map(url => (
+              <>
+                <a href={url.url}>
+                  <div className={"file link"}>
+                    {/* icon */}
+                    <img src={link}></img>
+                    {/* name */}
+                    <p className={"is-5"}>{url.description}</p>
+                  </div>
+                </a>
+              </>
+            ))
+          : null}
+      </div>
+    </>
+  )
+}
+
+export default Content

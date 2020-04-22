87c0d074502e9fb7bc7e51f25e063db161482a2e
allen <allen@localhost.localdomain>
2020-04-22 12:56:10 +0800

    fix something

--- src/templates/Article/Article.js
+++ src/templates/Article/Article.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../../components/layout/Layout"
+import Seo from "../../components/layout/Seo"
 import { Container, Row, Col } from "react-bootstrap"
 import { LazyLoadComponent } from "react-lazy-load-image-component"
 
@@ -15,32 +15,33 @@ import RecommandVideo from "../../components/tools/RecommandVideo/RecommandVideo
 export default ({ data }) => {
   return (
     <>
-      <Layout
+      <Seo
         title={data.allWordpressWpSpost.edges[0].node.title}
         description={data.allWordpressWpSpost.edges[0].node.content}
+        pathname={`/post/${data.allWordpressWpSpost.edges[0].node.wordpress_id}`}
         article
-      >
-        <div id="header-padding" />
-        <Container id="article" fluid>
-          <Row>
-            <Col lg="7">
-              <Content
-                title={data.allWordpressWpSpost.edges[0].node.title}
-                genre={data.allWordpressWpSpost.edges[0].node.acf.genre}
-                date={data.allWordpressWpSpost.edges[0].node.date}
-                content={data.allWordpressWpSpost.edges[0].node.content}
-                urls={data.allWordpressWpSpost.edges[0].node.acf.repeater_link}
-              />
-              <LazyLoadComponent>
-                <RecommandVideo />
-              </LazyLoadComponent>
-            </Col>
-            <Col lg={{ span: 4, offset: 1 }}>
-              <SideNews infinity={true} />
-            </Col>
-          </Row>
-        </Container>
-      </Layout>
+      />
+
+      <div id="header-padding" />
+      <Container id="article" fluid>
+        <Row>
+          <Col lg="7">
+            <Content
+              title={data.allWordpressWpSpost.edges[0].node.title}
+              genre={data.allWordpressWpSpost.edges[0].node.acf.genre}
+              date={data.allWordpressWpSpost.edges[0].node.date}
+              content={data.allWordpressWpSpost.edges[0].node.content}
+              urls={data.allWordpressWpSpost.edges[0].node.acf.repeater_link}
+            />
+            <LazyLoadComponent>
+              <RecommandVideo />
+            </LazyLoadComponent>
+          </Col>
+          <Col lg={{ span: 4, offset: 1 }}>
+            <SideNews infinity={true} />
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
@@ -53,6 +54,7 @@ export const query = graphql`
           title
           date
           content
+          wordpress_id
           acf {
             genre
             repeater_link {

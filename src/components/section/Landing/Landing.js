ab9b3ea9f477e79dffc34ee3695c3dc72c3704b5
allen <allen@localhost.localdomain>
2020-04-28 20:25:43 +0800

    fix something

--- src/components/section/Landing/Landing.js
+++ src/components/section/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Jumbotron from "react-bootstrap/Jumbotron"
+import { Jumbotron } from "react-bootstrap/Jumbotron"
 import { useStaticQuery, graphql } from "gatsby"
 
 // style
@@ -7,21 +7,23 @@ import "./Landing.scss"
 
 export const LandingPure = ({ headline }) => {
   return (
-    <Jumbotron id="landing" fluid>
-      <img
-        rel="preload"
-        src={headline.node.acf.image.sizes.wordpress_1536x1536}
-        alt={"Headline"}
-      ></img>
-      <div className={"fade-layer"}></div>
-      <a href={headline.node.acf.link}>
-        <h1 className={"is-1 serif bold"}>
-          <span className={"is-2"}>附中學生作品｜</span>
-          <br />
-          {headline.node.title}
-        </h1>
-      </a>
-    </Jumbotron>
+    <>
+      <Jumbotron className="landing" fluid>
+        <img
+          rel="preload"
+          src={headline.node.acf.image.sizes.wordpress_1536x1536}
+          alt={"Headline"}
+        ></img>
+        <div className={"fade-layer"}></div>
+        <a href={headline.node.acf.link}>
+          <h1 className={"is-1 serif bold"}>
+            <span className={"is-2"}>附中學生作品｜</span>
+            <br />
+            {headline.node.title}
+          </h1>
+        </a>
+      </Jumbotron>
+    </>
   )
 }
 const Landing = () => {

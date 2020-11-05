663ce47f7e2ba8c5098ef265774c3a9437a5f17b
Anmicius <anmicius01@protonmail.com>
2020-11-05 09:42:15 +0800

    menu source from file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: `國立臺灣師範大學附屬高級中學`,
-    titleTemplate: "%s | HSNU.org",
-    description: `國立臺灣師範大學附屬高級中學，簡稱台師大附中、師大附中、附中，官方英文簡稱為HSNU，是一所位處台北市中心、位於臺北市大安區信義路上的高級中學，附屬於國立臺灣師範大學。`,
+    titleTemplate: "%s | hsnu.org",
+    description: `國立臺灣師範大學附屬高級中學，簡稱為HSNU，是一所位處臺北市大安區信義的高級中學，附屬於國立臺灣師範大學。`,
     siteUrl: "https://hsnu.org", // No trailing slash allowed!
     image: "https://i.ytimg.com/vi/2fb-g_V-UT4/hqdefault.jpg",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-transition-link`,
+    `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-json`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `師大附中`,
-        short_name: `HSNU`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
-        display: `standalone`,
-        icon: `src/images/icons/HSNU.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-layout`,
-      options: {
-        component: require.resolve(`./src/components/layout/Layout.js`),
-      },
-    },
     // source from WordPress
     {
       resolve: `gatsby-source-wordpress-experimental`,
@@ -58,11 +40,38 @@ module.exports = {
         },
       },
     },
-    // sitemap
+    // source from file system
     {
-      resolve: `gatsby-plugin-sitemap`,
-      options: { exclude: [`/search`] },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
     },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout/Layout.js`),
+      },
+    },
+
+    ////////////////////////
+    //        SEO!!       //
+    ////////////////////////
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `師大附中`,
+        short_name: `HSNU`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+        icon: `src/images/icons/HSNU.png`,
+      },
+    },
+    `gatsby-plugin-advanced-sitemap`,
     // canonical link
     {
       resolve: `gatsby-plugin-canonical-urls`,
@@ -79,6 +88,7 @@ module.exports = {
         policy: [{ userAgent: "*", allow: "/" }],
       },
     },
+
     // google tag manager
     {
       resolve: "gatsby-plugin-google-tagmanager",
@@ -87,6 +97,7 @@ module.exports = {
         id: "GTM-WTBQCB3",
       },
     },
+
     // css
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
